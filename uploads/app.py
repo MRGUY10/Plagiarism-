68d0a565cee16fae3eadbadf3dea2a84a3718d24
MRGUY10 <kamdemjordan58@gmail.com>
2025-01-16 13:44:29 +0100

    update8

--- uploads/app.py
+++ uploads/app.py
@@ -16,7 +16,7 @@ app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), 'uploads')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16 MB max file size
 
 # Enable CORS after the app is created
-CORS(app, origins=["https://plagiarismhecker.vercel.app"])
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 SUPPORTED_LANGUAGES = {
     'py': 'Python',
